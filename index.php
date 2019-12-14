9cc5bbc750f91b6c7692b4c1d1472e7039da4274
Amadikwa Joy N <treasurechristain@gmail.com>
2019-12-14 06:38:46 -0800

    Update index.php

--- index.php
+++ index.php
@@ -10,18 +10,22 @@
       content="Event booking for events in Owerri Tech"
     />
     <title>OTEM</title>
+    <link href="static/fullcalendar/core/main.css" rel="stylesheet" />
+    <link href="static/fullcalendar/daygrid/main.css" rel="stylesheet" />
     <link rel="stylesheet" href="static/css/main.css" />
   </head>
   <body>
     <div class="wrapper">
       <div class="app">
-        <div class="date__banner"></div>
+        <div class="date__banner">
+          <div id="calendar"></div>
+        </div>
         <div class="events-container">
           <h4 class="events-title">All events for selected date</h4>
           <ul class="events">
             <li class="event">
               <div class="event__date">14th December, 2019</div>
-              <h3 class="event__name">GDG Owerri Event</h3>
+              <h3 class="event__name">Jack Event</h3>
               <h4 class="event__title">dignissimos excepturi?</h4>
               <p class="event__desc">
                 Lorem ipsum dolor sit amet consectetur adipisicing elit.
@@ -32,7 +36,7 @@
             </li>
             <li class="event">
               <div class="event__date">14th December, 2019</div>
-              <h3 class="event__name">Devc Owerri Event</h3>
+              <h3 class="event__name">Jack Event</h3>
               <h4 class="event__title">dignissimos excepturi?</h4>
               <p class="event__desc">
                 Lorem ipsum dolor sit amet consectetur adipisicing elit.
@@ -43,7 +47,7 @@
             </li>
             <li class="event">
               <div class="event__date">14th December, 2019</div>
-              <h3 class="event__name">ALC Event</h3>
+              <h3 class="event__name">Jack Event</h3>
               <h4 class="event__title">dignissimos excepturi?</h4>
               <p class="event__desc">
                 Lorem ipsum dolor sit amet consectetur adipisicing elit.
@@ -56,6 +60,21 @@
         </div>
       </div>
     </div>
+
+    <script src="static/fullcalendar/core/main.js"></script>
+    <script src="static/fullcalendar/daygrid/main.js"></script>
+
+    <script>
+      document.addEventListener("DOMContentLoaded", function() {
+        var calendarEl = document.getElementById("calendar");
+
+        var calendar = new FullCalendar.Calendar(calendarEl, {
+          plugins: ["dayGrid"]
+        });
+
+        calendar.render();
+      });
+    </script>
     <script src="static/js/main.js"></script>
   </body>
 </html>
